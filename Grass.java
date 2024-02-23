4eef8c3d9365adf7ce1942aeaca05ca26d8773ff
Sujin <sujinbonnaud@gmail.com>
2024-02-22 23:37:18 -0600

    create java proj

---
+++ Grass.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Grass here.
+ * 
+ * @Sujin Lee
+ * @1.0.0
+ */
+public class Grass extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class Grass.
+     * 
+     */
+    public Grass()
+    {
+    }
+}
